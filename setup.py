9fc2d4957fb6453171b709aa1947c28fd2859950
ParkSangJun <30718444+cosmoquester@users.noreply.github.com>
2021-05-01 16:05:37 +0900

    update: github username

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="This repository is template for my python project.",
     python_requires='>=3.7',
     install_requires=[],
-    url="https://github.com/psj8252/python3-template.git",
+    url="https://github.com/cosmoquester/python3-template.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
 )
